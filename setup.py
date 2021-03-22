785deb2dfcbee154438150682a59463510cbe8cb
Christian Koernig <ckoernig@mail.desy.de>
2021-03-22 10:46:57 +0100

    Fixed setup.py, removed autogenerated files from tracking, updated gitignore

--- setup.py
+++ setup.py
@@ -13,16 +13,16 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 # HexitecIO extension module
-_AmptekHardwareInterface = Extension("_AmptekHardwareInterface",
+_AmptekHardwareInterface = Extension("amptek_hardware_interface._AmptekHardwareInterface",
                    ["python/amptek_hardware_interface/AmptekHardwareInterface.i",] + ["src/" + f for f in os.listdir("src") if os.path.isfile("src/" + f)],
                    include_dirs = [numpy_include, "include"],
-                   swig_opts=['-c++',"-modern" , "-nomodernargs"],
+                   swig_opts=['-c++',"-modern", "-nomodernargs" ],
                    extra_compile_args = ["-std=c++11", "-g"], 
                    libraries=["usb-1.0"]
                    )
 
 # NumyTypemapTests setup
-setup(  name        = "AmptekHardwareInterface function",
+setup(  name        = "AmptekHardwareInterface",
         description = "Implementation of the Amptek DP5 Protocol",
         package_dir = {"amptek_hardware_interface": "python/amptek_hardware_interface"},
         packages    = ["amptek_hardware_interface"],
